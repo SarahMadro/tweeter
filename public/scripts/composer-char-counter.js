e81d668403580f53d37dacd37fd50f6378fb31c9
Sarah <sjmadro1@gmail.com>
2019-03-30 05:36:03 +0000

    changed css and added new screenshots

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,6 @@ $(`section.new-tweet textarea`).on(`input`, function(event) {
   event.stopPropagation();
   let $textAreaLeng = $(this).val().length;
   let $counter = $(this).parent().find('.counter');
-  // console.log('counter:', $counter);
   $counter.text(140 - $textAreaLeng);
   if($textAreaLeng > 140) {
     $counter.css(`color`, `red`);
